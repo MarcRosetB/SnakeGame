71169c884a022e56978266153308d7548dc8a65c
Marc Roset Bermudez <132048992+MarcRosetB@users.noreply.github.com>
2023-05-06 22:58:50 +0200

    añadidos el tablero y un archivo de utilidades

---
+++ SnakeGame/SnakeGame/Tablero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoSerpiente
+{
+    internal class Tablero
+    {
+        public readonly int Altura;
+        public readonly int Anchura;
+        public bool ContieneCaramelo;
+
+        public Tablero(int altura, int anchura)
+        {
+            Altura = altura;
+            Anchura = anchura;
+            ContieneCaramelo = false;
+        }
+        public void DibujarTablero()
+        {
+            for (int i = 0; i <= Altura; i++)
+            {
+                Utilidades.DibujarPosicion(Anchura, i, "#");
+                Utilidades.DibujarPosicion(0, i, "#");
+            }
+            for (int i = 0; i <= Anchura; i++)
+            {
+                Utilidades.DibujarPosicion(i, 0, "#");
+                Utilidades.DibujarPosicion(i, Altura, "#");
+            }
+        }
+    }
+}
---
+++ SnakeGame/SnakeGame/Utilidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoSerpiente
+{
+    internal static class Utilidades
+    {
+        public static void DibujarPosicion(int x, int y, string simbolo)
+        {
+            Console.SetCursorPosition(x, y);
+
+        }
+    }
+}
